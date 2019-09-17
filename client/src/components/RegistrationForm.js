7c1fdfc7a23073a824a9f788690221aa4ab1a0f2
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-09-17 12:04:52 -0400

    created base form for registration and handled the values successfully

--- client/src/components/RegistrationForm.js
+++ client/src/components/RegistrationForm.js
@@ -3,23 +3,43 @@ import React, { Component } from 'react'
 export default class RegistrationForm extends Component {
 
     state = {
-        regitser: {
-            name: "", age: 0,
-            state: "", email: "",
-            username: "", pin: 0000
+        register: {
+            name: String, age: Number,
+            state: String, email: String,
+            username: String, pin: Number
           }
+    }
+
+    handleInputValues = (evnt) => {
+        let register = {...this.state.register}
+
+        register[evnt.target.name] = evnt.target.value
+        
+        this.setState({ register })
+    }
+    
+    handleSubmit = (evnt) => {
+        evnt.preventDefault()
+
+        this.props.createNewUser(this.register)
     }
 
     render() {
         return (
             <div className="registration">
-                <form>
-                    <input type="text" name="name" />
-                    <input type="number" name="age" />
-                    <input type="text" name="state" />
-                    <input type="email" name="email" />
-                    <input type="text" name="username" />
-                    <input type="number" name="pin" />
+                <form onSubmit={this.handleSubmit}>
+                    <label>Name</label>
+                    <input type="text" name="name" onChange={this.handleInputValues}/>
+                    <label>Age</label>
+                    <input type="number" name="age" onChange={this.handleInputValues}/>
+                    <label>State</label>
+                    <input type="text" name="state" onChange={this.handleInputValues}/>
+                    <label>Email</label>
+                    <input type="email" name="email" onChange={this.handleInputValues}/>
+                    <label>Username</label>
+                    <input type="text" name="username" onChange={this.handleInputValues}/>
+                    <label>Pin</label>
+                    <input type="number" name="pin" onChange={this.handleInputValues}/>
                     <input type="submit" value="Create Account" />
                 </form>
             </div>
