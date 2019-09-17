707f6dafd5aea8d7333418fe7b54b5f6b0af5859
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-09-17 15:14:39 -0400

    created schema and collection with 3 functions

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('./connection.js')
+
+
+const UserSchema = new mongoose.Schema ({
+  username: {
+    type: String, 
+    required: true, 
+    unique: true, 
+    trim: true,
+    minlength: 3
+  },
+  pin: {
+    type: String, 
+    required: true, 
+    unique: true, 
+    maxlength: 4
+  }
+})
+
+
+const UserCollection = mongoose.model('User', UserSchema)
+
+
+const getUsers = () => (UserCollection.find())
+
+const addUser = (newUser) => (UserCollection.insertMany([newUser]))
+
+const getUser = (id) => (UserCollection.findById(id))
+
+
+module.exports = {
+  addUser,
+  getUser,
+  getUsers
+}
