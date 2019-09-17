e95754db3b5194ed425a3f1a227a55c98da525a2
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-09-17 13:50:35 -0400

    created form to create user credentials

---
+++ client/src/components/UserCredentialsForm.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+export default class UserCredentialsForm extends Component{
+    state = {
+        user: {username: String, Pin: Number}
+    }
+
+
+    handleInputValues = (evnt) => {
+        let user = {...this.state.user}
+
+        user[evnt.target.name] = evnt.target.value
+
+        this.setState({ user })
+    }
+
+    handleSubmit = (evnt) => {
+        evnt.preventDefault()
+
+        this.props.createNewUser(this.state.user)
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Username</label>
+                    <input type="text" name="username" onChange={this.handleInputValues}/>
+                    <label>Pin</label>
+                    <input type="number" name="pin" onChange={this.handleInputValues}/>
+                    <input type="submit" value="Finish" />
+                </form>
+            </div>
+        )
+    }
+}
