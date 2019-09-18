261300f3f359ce07ac2a607ceec13bf2f216cf0c
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-09-18 17:50:28 -0400

    completed controller but need testing

--- controllers/registration.js
+++ controllers/registration.js
@@ -2,11 +2,43 @@ const express = require('express')
 const Registration = require('../models/registration')
 const router = express.Router()
 
-router.route('/').get((req,res) => {
-    Registration.find()
-      .then(users => res.json(users))
+router.route('/').get((req, res) => {
+  Registration.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json('Error: ' + err))
+})
+
+router.route('/add').post((req, res) => {
+  const name = req.body.name
+  const age = Number(req.body.age)
+  const state = req.body.state
+  const email = req.body.email
+
+  const newRegistration = new Registration({ 
+    name, 
+    age, 
+    state, 
+    email })
+
+    newRegistration.save()
+      .then(() => res.json('Successfully Registered'))
       .catch(err => res.status(400).json('Error: ' + err))
-  })
+})
+
+router.route('edit/:id').put((req,res) => {
+  Registration.findById(req.params.id)
+    .then(register => {
+      register.name = req.body.name
+      register.age = Number(req.body.age)
+      register.state = req.body.state
+      register.email = req.body.email
+
+      register.save()
+        .then(() => res.json('Registration Update'))
+        .catch(err => res.send(400).json('Error: ' + err))
+    })
+
+})
 
 
 module.exports = Registration
