dcc79ad794d2df6a6a94bb5f3466d291b6f6c1e1
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-09-20 09:08:30 -0400

    updated get route

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
-const app = express()
-const cors = require('cors')
 const express = require('express')
+const app = express()
 const methodOverride = require('method-override')
-const userRouter = require('./controllers/user.js')
-const noteRouter = require('./controllers/note.js')
-const registrationRouter = require('./controllers/registration.js')
+const cors = require('cors')
 
+app.use(express.urlencoded({ extended: true }))
 
 app.use(cors())
 app.use(express.json())
 app.use(methodOverride('_method'))
-app.use(express.urlencoded({ extended: true }))
+
+
 app.use(express.static(__dirname + "/client/build"))
-app.get('/*', (req, res) => {
+
+
+app.get('/journal', (req, res) => {
     res.sendFile(`${__dirname}/client/build/index.html`)
 })
+
+
+//Routers & Routes
+const userRouter = require('./controllers/user.js')
+const noteRouter = require('./controllers/note.js')
+const registrationRouter = require('./controllers/registration.js')
+
 app.use('/user', userRouter)
 app.use('/note', noteRouter)
 app.use('/registration', registrationRouter)
+
+
 const PORT = process.env.PORT || 3000
 
 
