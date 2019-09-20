76f0a5ec3a9409a558b826ae65b9815107516794
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-09-20 09:06:02 -0400

    organized server file

--- server.js
+++ server.js
@@ -1,34 +1,23 @@
-const express = require('express')
 const app = express()
-const methodOverride = require('method-override')
 const cors = require('cors')
-
-app.use(express.urlencoded({ extended: true }))
-
-app.use(cors())
-app.use(express.json())
-
-app.use(methodOverride('_method'))
-
-
-app.use(express.static(__dirname + "/client/build"))
-
-
-app.get('/*', (req, res) => {
-    res.sendFile(`${__dirname}/client/build/index.html`)
-})
-
-
-//Routers & Routes
+const express = require('express')
+const methodOverride = require('method-override')
 const userRouter = require('./controllers/user.js')
 const noteRouter = require('./controllers/note.js')
 const registrationRouter = require('./controllers/registration.js')
 
+
+app.use(cors())
+app.use(express.json())
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(__dirname + "/client/build"))
+app.get('/*', (req, res) => {
+    res.sendFile(`${__dirname}/client/build/index.html`)
+})
 app.use('/user', userRouter)
 app.use('/note', noteRouter)
 app.use('/registration', registrationRouter)
-
-
 const PORT = process.env.PORT || 3000
 
 
