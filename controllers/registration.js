247289ba931c7f453e0c5e126bb099813dd3e84f
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-09-20 21:42:16 -0400

    successfully tested and completed MVP

--- controllers/registration.js
+++ controllers/registration.js
@@ -8,7 +8,7 @@ router.route('/').get((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err))
 })
 
-router.route('/add').post((req, res) => {
+router.route('/').post((req, res) => {
   const name = req.body.name
   const age = Number(req.body.age)
   const state = req.body.state
