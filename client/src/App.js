3a4dcefdb8c86ed2d44f468c5016c5d43d97db1b
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-09-23 15:14:27 -0400

    cleaned code

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
 //File Imports
 import React from 'react';
 import 'antd/dist/antd.css'
-// import { Typography } from 'antd';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-// import { Layout, Menu, Breadcrumb, Icon } from 'antd';
 import './App.css'
 
 //Component Imports
@@ -13,12 +11,7 @@ import UserCredentialsForm from './components/UserCredentialsForm'
 import Journal from './components/Journal'
 import HomePage from './components/HomePage'
 
-//Layout
-// const { Header, Content, Footer, Sider } = Layout;
-// const { Title } = Typography;
-// const { SubMenu } = Menu;
 
-//Code
 
 class App extends React.Component {
 
@@ -26,10 +19,10 @@ class App extends React.Component {
     return (
       <Router>
         <Switch>
-          <Route exact path="/home" component={HomePage} />
+          {/* <Route exact path="/home" component={HomePage} /> */}
           <Route exact path="/registration/2" component={UserCredentialsForm} />
           <Route exact path="/registration/1" component={RegistrationForm} />
-          <Route exact path="/journal" component={Journal} />
+          {/* <Route exact path="/journal" component={Journal} /> */}
           <Route exact path="/note" component={NoteForm} />
           <HomePage />
         </Switch>
